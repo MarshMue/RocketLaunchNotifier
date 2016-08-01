61c105ae94c6a4ba0fb9e84a4c9b848a22a10568
Marshall Mueller <mnmueller@wisc.edu>
2016-07-31 20:05:29 -0500

    removed telebot and renamed launchbot

---
+++ launchbot.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	//"database/sql"
+	//_ "github.com/mattn/go-sqlite3"
+	//"gopkg.in/telegram-bot-api.v4"
+	"fmt"
+	//"log"
+	"github.com/yhat/scrape"
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
+	"net/http"
+	"container/list"
+)
+
+type launch struct{
+	date string
+	time string
+	site string
+	rocket string
+	payload string
+	description string
+}
+
+func getLaunchInfo() launch {
+	// init list of launches
+	launches := list.New()
+
+	
+
+	resp, err := http.Get("http://spaceflightnow.com/launch-schedule/")
+	if err != nil {
+		panic(err)
+	}
+	root, err := html.Parse(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+
+	// define a matcher
+	matcher := func(n *html.Node) bool {
+		// must check for nil values
+		if n.DataAtom == atom.Div {
+			return scrape.Attr(n, "class") == "entry-content clearfix"
+		}
+		return false
+	}
+
+	// grab all launch dates
+	dates := scrape.FindAll(root, matcher)
+	for _, date := range dates {
+		fmt.Printf("Date: %s\n", scrape.Text(date))
+	}
+	fmt.Printf("Length of dates: %d\n", len(dates))
+
+	// grab all missions
+	missions := scrape.FindAll(root, matcherMission)
+	for _, mission := range missions {
+		fmt.Printf("Mission: %s\n", scrape.Text(mission))
+	}
+	fmt.Printf("Length of missions: %d\n", len(missions))
+}
+
+func main() {
+	/*
+		bot, err := tgbotapi.NewBotAPI("249474117:AAHPf1gj7olfzcEs9WmmMPSW34eHM8FF32s")
+		if err != nil {
+			log.Panic(err)
+		}
+
+		bot.Debug = true
+
+		log.Printf("Authorized on account %s", bot.Self.UserName)
+
+		u := tgbotapi.NewUpdate(0)
+		u.Timeout = 60
+
+		updates, err := bot.GetUpdatesChan(u)
+
+		for update := range updates {
+			if update.Message == nil {
+				continue
+			}
+
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+
+
+
+		db, err := sql.Open("sqlite3", "./launchbot.db")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer db.Close()
+
+		_, err = db.Exec("insert into launches(Date, Rocket, Payload, Description) values('07-30-2016', 'Falcon 9', 'GOES-R', 'Sample')")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		rows, err := db.Query("select Date, Rocket, Payload, Description from launches")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for rows.Next() {
+			var date string
+			var rocket string
+			var payload string
+			var desc string
+			err = rows.Scan(&date, &rocket, &payload, &desc)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(date, rocket, payload, desc)
+		}
+		err = rows.Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+	*/
+
+
+}
